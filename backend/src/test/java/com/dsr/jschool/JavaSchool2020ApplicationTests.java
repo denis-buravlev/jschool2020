5815e109c325e5fbba3200a19ffda7b37e7b5623
Denis Buravlev <denbur93@gmail.com>
2020-09-30 20:45:57 +0300

    Lesson 1: Initial commit

---
+++ backend/src/test/java/com/dsr/jschool/JavaSchool2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsr.jschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSchool2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
